3f7e3514de41d2bd76cd09e8a2e4d293f43ff85a
jgdisenio <j.gazzin@gmail.com>
2025-12-09 17:23:03 -0300

    galeria

--- build/js/app.js
+++ build/js/app.js
@@ -2,56 +2,121 @@ const anio = document.querySelector('.anio');
 const anioActual = new Date();
 anio.textContent = anioActual.getFullYear();
 
-// imprimir galeria
-const $$galeria = document.querySelector('#galeria .galeria-imagenes');
-const galeriaImgs = [
-    '1.png',
-    '2.png',
-    '3.png',
-    '5.png',
-    '6.png',
-    '8.png',
-    '9.png',
-    '10.png',
-    '11.png'
-]
+// galeria
+let cat = 'reformas';
+let imagenActual = 1;
 
+let imagenes = [];
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    imprimirGaleria(galeriaImgs);
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarImagen(imagenActual);
+});
 
+// categoria
+const categorias = document.querySelectorAll('.categorias p');
+categorias.forEach(item => {
+    item.addEventListener('click', 
+        (e) => {
+        cat = e.target.dataset.cat;
+        console.log(cat);
+        imagenActual = 1;
+        mostrarImagen(imagenActual);
+    });
 });
 
 
-function imprimirGaleria(imagenes) {
-    const listado_imagenes = imagenes;
-    let galeriaId = 0;
-    setInterval(() => {
-    $$galeria.innerHTML = '';
-    const $img = document.createElement('img');
-    $img.src = `build/img/galeria/${listado_imagenes[galeriaId]}`;
-    $$galeria.appendChild($img);
-    galeriaId++;
-        if (galeriaId >= listado_imagenes.length) {
-            galeriaId = 0;
+// dots
+const dots = document.querySelectorAll('.dot');
+dots.forEach(dot => {
+    dot.addEventListener('click', 
+        (e) => {
+        if(dot === e.target){
+            dots.forEach(d => d.classList.remove('dot_active'));                            
+            
+            e.target.classList.add('dot_active');
+
+            imagenActual = e.target.dataset.img;
+            console.log(imagenActual);
+            mostrarImagen(imagenActual);
+            return
         }
-    
-    }, 2500);
+        
+    });
+});
+
+
+// next prev
+const prev = document.querySelector('#galeria .fa-angle-left');
+const next = document.querySelector('#galeria .fa-angle-right');
+
+prev.addEventListener('click', () => {
+    imagenActual--;
+    if(imagenActual <1) imagenActual = 3;
+    const dotActual = document.querySelector(`.dot[data-img='${imagenActual}']`);
+    dots.forEach(d => d.classList.remove('dot_active'));                            
+    dotActual.classList.add('dot_active');
+    mostrarImagen(imagenActual);
+});
+
+next.addEventListener('click', () => {
+    imagenActual++;
+    if(imagenActual >3) imagenActual = 1;
+    const dotActual = document.querySelector(`.dot[data-img='${imagenActual}']`);
+    dots.forEach(d => d.classList.remove('dot_active'));                            
+    dotActual.classList.add('dot_active');
+    mostrarImagen(imagenActual);
+});
+
+function mostrarImagen(num) {
+   const contenedorImagen = document.querySelector('.galeria-imagenes'); 
+   contenedorImagen.innerHTML = '';
+   const imgNum = parseInt(num)-1;
+
+   console.log(cat);
+   console.log(num);
+
+   const img = document.createElement('img');
+   if(cat === 'reformas'){
+        imagenes = [
+            '1.png',
+            '2.png',
+            '3.png',
+        ]
+        img.src = `build/img/galeria/${imagenes[imgNum]}`;
+        contenedorImagen.appendChild(img);
+   } else if(cat === 'obras'){
+        imagenes = [
+            '4.png',
+            '5.png',
+            '6.png',
+        ]
+        img.src = `build/img/galeria/${imagenes[imgNum]}`;
+        contenedorImagen.appendChild(img);
+   } else if(cat === 'muebles'){
+        imagenes = [
+            '7.png',
+            '8.png',
+            '9.png',
+        ]
+        img.src = `build/img/galeria/${imagenes[imgNum]}`;
+        contenedorImagen.appendChild(img);
+   }
 }
 
-// FAQS: click para fijar open/close y hover para mostrar temporalmente
+
+
+
+// FAQS
 const items = document.querySelectorAll('.item');
 console.log(items);
 items.forEach(item => {
     const icons = item.querySelectorAll('.fa-solid');
-    // click en la flecha o en la pregunta
+
     icons.forEach(icon => {
         icon.addEventListener('mouseover', (e) => {
             toggleItem(item, icon);
         });
-        icon.addEventListener('mouseleave', (e) => {
-            toggleItem(item, icon);
-        });
+
 
     });
 });
@@ -71,5 +136,5 @@ function toggleItem(item, icon) {
     if (isOpen) {
         icon.classList.toggle('fa-angle-down');
         icon.classList.toggle('fa-angle-up');
-    }
+    };
 };
